5a064c71694579dcc4c911fe4368dced7ac2254f
Ken <khuynh024@gmail.com>
2018-04-04 22:02:53 -0500

    v1

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,48 +1,122 @@
 
 
+//Create WordBank Array
+var wordBankArr = ["mew","pikachu","squirtle", "dragonite", "flareon", "umbreon", "dugtrio","psyduck", "zapdos", "moltres", "meowth", "onyx", "rhydon", "pidgey", "spheal", "gastly"]
+//Generate Random Number based off length of box
+var rng = Math.floor(Math.random()* wordBankArr.length);
+var chosenWord = wordBankArr[rng];
+
+//Create empty Arrays
+var underScoreArr = [];
+var rightLettersArr = [];
+var wrongLettersArr = [];
+
+//Creat Counter variables
+var wins = 0;
+var losses = 0;
+var guesses = 10;
+
+//Create State Of Game
+var gameState = false;
+
 //functions
-function checkLength () {
-    console.log(underScore);
-    console.log(underScore.length/2);
-
+function check(){
+    // console.log("Game Started");
+    // console.log("rng: "+ rng);
+    console.log("chosenWord: "+ chosenWord);
+    console.log("UnderScores: "+ underScoreArr);
+    console.log("Wrong letters " + wrongLettersArr);
+    console.log("Right letters " + rightLettersArr);
 }
 
-//Logic
-
-//Create Word Box Array
-var wordBox = ["mew","lapras","pikachu","squirtle", "charmander"]
-let correctArr = [];
-let buHaoArr = [];
-//Pull Word from Word Box Array
-let rng = Math.floor(Math.random()* wordBox.length);
-let chosenOne = wordBox[rng];
-console.log(chosenOne);
-
-
-//create underscores based on length of word
-let underScore = [];
-
-function generateUnderscores () {
-    for (let i=0; i < chosenOne.length; i++) {
-        underScore = underScore + "_ "
-        
+//function to push underscores 
+function generateUnderscores(){
+    for (var i = 0; i < chosenWord.length; i++){
+        underScoreArr.push(" _ ");
     }
-}
-
-
-generateUnderscores();
-checkLength();
-
-//Get userInput 
-document.onkeyup = function(event){
-    var userChoice = event.key.toLowerCase();
-    console.log(userChoice);
-
+    return underScoreArr;
 
 }
 
 
-//Check userInput is correct
-//if correct go to right array
-//if wrong go to wrong array
-//Once word is guessed, rng word
+
+//Main Function
+function gameStart(){
+    underScoreArr = [];
+    rightLettersArr = [];
+    wrongLettersArr = [];
+    guesses = 10;
+    gameState = true;
+    rng = Math.floor(Math.random()* wordBankArr.length);
+    chosenWord = wordBankArr[rng];
+    generateUnderscores();
+    document.getElementById("underscores").textContent= [underScoreArr.join(" ")];
+    document.getElementById("correctGuesses").textContent= "Correct Guesses: ";
+    document.getElementById("wrongGuesses").textContent= "Wrong Guesses: ";
+    document.getElementById("lives").textContent= ["Lives Left: "+ guesses];
+
+
+
+
+//user input function
+document.onkeypress = function (event) {
+    var keyword = String.fromCharCode(event.keyCode);
+
+    if (chosenWord.indexOf(keyword) > -1){
+        rightLettersArr.push(keyword);
+          underScoreArr[chosenWord.indexOf(keyword)] = keyword;
+            
+        //prints letter into underscores
+        document.getElementById("underscores").textContent= [underScoreArr.join(" ")];
+        document.getElementById("correctGuesses").textContent= ["Correct Guesses: " + rightLettersArr];
+
+        //place letters in correct location of word
+        function correctGuess(event) {
+            for (var j = 0; j < chosenWord.length; j++) {
+                if (chosenWord[j] === keyword) {
+                    underScoreArr[j] =  keyword;
+                }
+            }            
+        }
+    
+
+            if (underScoreArr.join("") == chosenWord){
+                alert("You Win!");
+                wins++;
+                document.getElementById("wins").textContent= ["Wins: "+ wins];
+                gameStart();
+            }
+
+    }
+
+    else {
+        wrongLettersArr.push(keyword);
+        document.getElementById("wrongGuesses").textContent= ["Wrong Guesses: "+ wrongLettersArr];
+        guesses--;
+        document.getElementById("lives").textContent= ["Lives Left: "+ guesses];
+
+        //if game over alert!
+        if (guesses === 0){
+            alert("You are out of guesses! Game Over!")
+            losses++;
+            document.getElementById("losses").textContent= ["Losses: "+ losses];
+            gameStart();
+        }
+
+
+
+    }
+    
+    check();
+
+};
+
+
+
+
+}
+
+
+
+
+
